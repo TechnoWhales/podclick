65b0bc8a7bb00019e3978502c47e36f36ac9c50e
Petro Sahal(hhgtl) <petrosahal66@gmail.com>
2025-05-30 14:04:20 +0300

    INC1-168 refactor: add explicit type for iconId prop in Icon

--- src/components/ui/Icon/Icon.tsx
+++ src/components/ui/Icon/Icon.tsx
@@ -1,18 +1,21 @@
-type IconPropsType = {
-  iconId: string
+import { IconsId } from '@/types/Icons'
+
+type IconProps = {
+  iconId: IconsId
+  fill?: string
   width?: string
   height?: string
   viewBox?: string
   gradient?: string
 }
 
-const Icon = ({ iconId, height, viewBox, width }: IconPropsType) => {
+const Icon = ({ iconId, fill, height, viewBox, width }: IconProps) => {
   return (
     <svg
       width={width || '24'}
       height={height || '24'}
       viewBox={viewBox || '0 0 24 24'}
-      fill={'none'}
+      fill={fill || 'none'}
       xmlns={'http://www.w3.org/2000/svg'}
     >
       <use href={`/icons-sprite.svg#${iconId}`} />
