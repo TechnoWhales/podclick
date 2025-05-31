90c6e073cb0e090255a4f636928ab5df9ac88898
ElenaNam <zaidelsonelena@gmail.com>
2025-05-31 20:39:35 +0400

    INC1-170 feat: add typography

---
+++ src/components/ui/typography/Typography.tsx
@@ -0,0 +1,32 @@
+import { ComponentPropsWithoutRef, ElementType, ReactNode } from 'react'
+import s from './Typography.module.scss'
+
+export type TypographyVariant =
+  | 'large'
+  | 'h1'
+  | 'h2'
+  | 'h3'
+  | 'regular_text_16'
+  | 'bold_text_16'
+  | 'regular_text_14'
+  | 'medium_text_14'
+  | 'bold_text_14'
+  | 'small_text'
+  | 'semibold_small_text'
+  | 'regular_link'
+  | 'small_link'
+  | 'error'
+
+export type TypographyProps<T extends ElementType = 'p'> = {
+  as?: T
+  children: ReactNode
+  variant?: TypographyVariant
+  className?: string
+  color?: string
+} & ComponentPropsWithoutRef<T>
+
+export const Typography = <T extends ElementType = 'p'>(props: TypographyProps<T>) => {
+  const { variant = 'regular_text_14', className = '', as: Component = 'p', ...rest } = props
+
+  return <Component className={`${s[variant]} ${className}`} {...rest} />
+}
