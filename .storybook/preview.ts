6e3d92b8969906cc3f6bc4ac44fff9e259078a44
ElenaNam <zaidelsonelena@gmail.com>
2025-06-03 22:08:18 +0400

    INC1-181 feat: add storybook settings

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,14 +1,32 @@
 import type { Preview } from '@storybook/react'
+import '@/shared/styles/index.scss'
 
 const preview: Preview = {
   parameters: {
     controls: {
       matchers: {
-       color: /(background|color)$/i,
-       date: /Date$/i,
+        color: /(background|color)$/i,
+        date: /Date$/i,
       },
     },
+    backgrounds: {
+      default: 'dark',
+      values: [
+        {
+          name: 'dark',
+          value: 'var(--color-dark-900)',
+        },
+        {
+          name: 'gray',
+          value: 'var(--color-dark-100)',
+        },
+        {
+          name: 'light',
+          value: 'var(--color-light-100)',
+        },
+      ],
+    },
   },
-};
+}
 
-export default preview;
+export default preview
