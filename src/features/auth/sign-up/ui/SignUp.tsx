f52188ee256ab13ba67fedda0328ffcf7e479675
Petro Sahal <petrosahal66@gmail.com>
2025-06-12 01:18:35 +0300

    INC1-178 feet: add text: 'Do you have an account' and link sign in

--- src/features/auth/sign-up/ui/SignUp.tsx
+++ src/features/auth/sign-up/ui/SignUp.tsx
@@ -19,7 +19,13 @@ export const SignUp = () => {
     setValue,
   } = useForm<Inputs>({
     resolver: zodResolver(signUpSchema),
-    defaultValues: { username: '', email: '', password: '', agreePolicy: false },
+    defaultValues: {
+      username: '',
+      email: '',
+      password: '',
+      confirmPassword: '',
+      agreePolicy: false,
+    },
   })
 
   const inputMargin = '0 0 24px'
@@ -47,15 +53,17 @@ export const SignUp = () => {
           margin={inputMargin}
           label={'Password'}
           error={errors.password?.message}
+          variant={errors.password?.message ? 'horizontalBorders' : 'fullBorders'}
           mode={'password'}
           {...register('password')}
         />
         <TextField
-          margin={inputMargin}
-          label={'Password'}
-          error={errors.password?.message}
+          margin={'0, 0, 12px, 0'}
+          label={'Password confirmation'}
+          error={errors.confirmPassword?.message}
+          variant={errors.confirmPassword?.message ? 'horizontalBorders' : 'fullBorders'}
           mode={'password'}
-          {...register('password')}
+          {...register('confirmPassword')}
         />
         <div className={s.policyWrapper}>
           <Controller
@@ -67,17 +75,23 @@ export const SignUp = () => {
           />
           <div className={s.policyText}>
             <Typography variant={'small_text'}>I agree to the</Typography>
-            <Typography variant={'small_link'} as={'a'}>
+            <Typography variant={'small_link'} as={'a'} href={'/auth/TermsOfService'}>
               Terms of Service
             </Typography>
             <Typography variant={'small_text'}>and</Typography>
-            <Typography variant={'small_link'} as={'a'}>
+            <Typography variant={'small_link'} as={'a'} href={'/auth/PrivacyPolicy'}>
               Privacy Policy
             </Typography>
           </div>
         </div>
-        <Button type={'submit'} fullwidth>
+        <Button className={s.signUpBtn} type={'submit'} fullwidth>
           Sign up
+        </Button>
+        <Typography className={s.signInTitle} variant={'regular_text_16'}>
+          Do you have an account?
+        </Typography>
+        <Button className={s.signInBtn} as={'a'} variant={'link'} href={'/auth/sign-in'}>
+          Sign In
         </Button>
       </form>
     </Card>
