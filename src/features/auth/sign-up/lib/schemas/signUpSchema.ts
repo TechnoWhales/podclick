a2615379c82ff4c12cacbd4af9531352063a542c
Petro Sahal <petrosahal66@gmail.com>
2025-06-11 18:05:41 +0300

    INC1-178 feet: add username validation in Schema

--- src/features/auth/sign-up/lib/schemas/signUpSchema.ts
+++ src/features/auth/sign-up/lib/schemas/signUpSchema.ts
@@ -1,16 +1,28 @@
 import { z } from 'zod'
 
-export const signUpSchema = z.object({
-  username: z.string().min(1, { message: 'Username is required' }),
-  email: z
-    .string()
-    .min(1, { message: 'Email is required' })
-    .email({ message: 'Incorrect email address' }),
-  password: z
-    .string()
-    .min(1, { message: 'Password is required' })
-    .min(3, { message: 'Password must be at least 3 characters long' }),
-  agreePolicy: z.boolean(),
-})
+export const signUpSchema = z
+  .object({
+    username: z
+      .string()
+      .min(1, { message: 'Username is required' })
+      .max(30, { message: 'Maximum number of characters 30' })
+      .regex(/^[A-Za-z0-9_-]+$/, {
+        message: 'Username must contain 0-9, A-Z, a-z, _, -',
+      }),
+    email: z
+      .string()
+      .min(1, { message: 'Email is required' })
+      .email({ message: 'Incorrect email address' }),
+    password: z
+      .string()
+      .min(1, { message: 'Password is required' })
+      .min(6, { message: 'Minimum number of characters 6' }),
+    confirmPassword: z.string(),
+    agreePolicy: z.boolean(),
+  })
+  .refine(data => data.password === data.confirmPassword, {
+    message: 'Passwords do not match',
+    path: ['confirmPassword'],
+  })
 
 export type Inputs = z.infer<typeof signUpSchema>
