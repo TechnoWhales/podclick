f64b3fb098347dbf3fbde738eb24af7090791c4c
Petro Sahal <petrosahal66@gmail.com>
2025-06-13 01:43:32 +0300

    INC1-178 feet: finish layout for EmailVerified component

--- src/features/auth/sign-up/ui/email-verification/email-verified/EmailVerified.tsx
+++ src/features/auth/sign-up/ui/email-verification/email-verified/EmailVerified.tsx
@@ -1,3 +1,26 @@
+'use client'
+import Image from 'next/image'
+
+import { Button, Container, Typography } from '@/shared/components/ui'
+
+import s from './EmailVerified.module.scss'
+
 export const EmailVerified = () => {
-  return <div>EmailVerified</div>
+  return (
+    <Container className={s.container} width={432}>
+      <Typography variant={'h1'} style={{ marginBottom: '20px' }}>
+        Congratulations!
+      </Typography>
+      <Typography variant={'regular_text_16'} style={{ marginBottom: '54px' }}>
+        Your email has been confirmed
+      </Typography>
+      <Button style={{ marginBottom: '72px' }}>Sign in</Button>
+      <Image
+        src={'/woman-leaning-against-the-wall.svg'}
+        alt={'woman leaning against the wall'}
+        width={432}
+        height={300}
+      />
+    </Container>
+  )
 }
