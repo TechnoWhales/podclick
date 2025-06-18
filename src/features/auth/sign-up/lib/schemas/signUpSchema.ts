46d9ceb39c0c2ebf6bb20c98e2d4e3326ed12f4f
Petro Sahal <petrosahal66@gmail.com>
2025-06-18 18:23:38 +0300

    INC1-287 refactor: add password validation for uppercase, digit, special character and add username minimum length validation

--- src/features/auth/sign-up/lib/schemas/signUpSchema.ts
+++ src/features/auth/sign-up/lib/schemas/signUpSchema.ts
@@ -5,6 +5,7 @@ export const signUpSchema = z
     userName: z
       .string()
       .min(1, { message: 'Username is required' })
+      .min(6, { message: 'Minimum number of characters 6' })
       .max(30, { message: 'Maximum number of characters 30' })
       .regex(/^[A-Za-z0-9_-]+$/, {
         message: 'Username must contain 0-9, A-Z, a-z, _, -',
@@ -18,9 +19,19 @@ export const signUpSchema = z
       .min(1, { message: 'Password is required' })
       .min(6, { message: 'Minimum number of characters 6' })
       .max(20, { message: 'Maximum number of characters 20' })
-      .regex(/^[A-Za-z0-9!"#$%&'()*+,\-./:;<=>?@[\\\]^_`{|}~]+$/, {
+      .regex(/^[A-Za-z0-9!"#$%&'()*+,\-./:;<=>?@[\\\]^_{|}~]+$/, {
         message:
-          'Password must contain a-z, A-Z,  ! " # $ % & \' ( ) * + , - . / : ; < = > ? @ [ \\ ] ^ _` { | } ~',
+          'Password must contain a-z, A-Z,  ! " # $ % & \' ( ) * + , - . / : ; < = > ? @ [ \\ ] ^ _ { | } ~',
+      })
+      .refine(value => /[A-Z]/.test(value), {
+        message: 'Password must contain at least one uppercase letter',
+      })
+      .refine(value => /[0-9]/.test(value), {
+        message: 'Password must contain at least one number',
+      })
+      .refine(value => /[!"#$%&'()*+,\-./:;<=>?@[\\\]^_`{|}~]/.test(value), {
+        message:
+          'Password must contain at least one special character: ! " # $ % & \' ( ) * + , - . / : ; < = > ? @ [ \\ ] ^ _ ` { | } ~',
       }),
     confirmPassword: z.string(),
     agreePolicy: z.boolean().refine(val => val === true, {
