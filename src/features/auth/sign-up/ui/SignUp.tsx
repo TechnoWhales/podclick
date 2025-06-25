282592ae60c6eba35cd266d27cf524d84e889d16
Petro Sahal <petrosahal66@gmail.com>
2025-06-25 21:55:55 +0300

    INC1-288 feat: add internationalization to SignUp component

--- src/features/auth/sign-up/ui/SignUp.tsx
+++ src/features/auth/sign-up/ui/SignUp.tsx
@@ -5,6 +5,7 @@ import { Controller, SubmitHandler, useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { useTranslations } from 'next-intl'
 import Link from 'next/link'
+import { z } from 'zod'
 
 import { OAuth } from '@/features/auth'
 import { useRegistrationMutation } from '@/features/auth/sign-up/api/signUpApi'
@@ -13,6 +14,7 @@ import { Button, Card, TextField, Typography } from '@/shared/components/ui'
 import { Checkbox } from '@/shared/components/ui/checkbox/Checkbox'
 import { Modal } from '@/shared/components/ui/modal/Modal'
 import { ROUTES } from '@/shared/constans'
+import { useMakeZodI18nMapForAuth } from '@/shared/hooks/useMakeZodI18nMapForAuth'
 import { RTKQueryError } from '@/shared/types/Response'
 
 import s from './SignUp.module.scss'
@@ -20,10 +22,15 @@ import s from './SignUp.module.scss'
 const inputMargin = '0 0 24px'
 
 export const SignUp = () => {
+  const authZodErrors = useMakeZodI18nMapForAuth()
+
+  z.setErrorMap(authZodErrors) // Переопределяем сообщения об ошибках в Zod для этой компоненты
   const [email, setEmail] = useState('')
   const [isOpened, setIsOpened] = useState(false)
 
-  const t = useTranslations('SignUp')
+  const tAuth = useTranslations('signUp')
+  const tCommon = useTranslations('common')
+
   const [registration] = useRegistrationMutation()
 
   const {
@@ -73,50 +80,54 @@ export const SignUp = () => {
 
   return (
     <Card flex={'columnCenter'} className={s.card}>
-      <Modal open={isOpened} onClose={() => setIsOpened(false)} modalTitle={'Email sent'}>
+      <Modal
+        open={isOpened}
+        onClose={() => setIsOpened(false)}
+        modalTitle={tAuth('emailSent.title')}
+      >
         <div>
           <Typography variant={'regular_text_16'}>
-            We have sent a link to confirm your email to {email}
+            {tAuth('emailSent.message', { email })}
           </Typography>
           <Button className={s.modalBtn} onClick={() => setIsOpened(false)}>
-            OK
+            {tCommon('button.ok')}
           </Button>
         </div>
       </Modal>
       <form onSubmit={handleSubmit(onSubmit)}>
         <div className={s.oAuthWrapper}>
           <Typography variant={'h1'} as={'h1'}>
-            {t('title')}
+            {tAuth('title')}
           </Typography>
           <OAuth />
         </div>
         <TextField
-          placeholder={'Username'}
+          placeholder={tAuth('username.placeholder')}
           margin={errors.userName?.message ? '0' : inputMargin}
-          label={'Username'}
+          label={tAuth('username.label')}
           error={errors.userName?.message}
           {...register('userName')}
         />
         <TextField
-          placeholder={'Email@gmail.com'}
+          placeholder={tAuth('email.placeholder')}
           margin={errors.email?.message ? '0' : inputMargin}
-          label={'Email'}
+          label={tAuth('email.label')}
           error={errors.email?.message}
           {...register('email')}
         />
         <TextField
-          placeholder={'Password'}
+          placeholder={tAuth('password.placeholder')}
           margin={errors.password?.message ? '0' : inputMargin}
-          label={'Password'}
+          label={tAuth('password.label')}
           error={errors.password?.message}
           variant={errors.password?.message ? 'horizontalBorders' : 'fullBorders'}
           mode={'password'}
           {...register('password')}
         />
         <TextField
-          placeholder={'Password confirmation'}
+          placeholder={tAuth('confirmPassword.placeholder')}
           margin={errors.confirmPassword?.message ? '0' : inputMargin}
-          label={'Password confirmation'}
+          label={tAuth('confirmPassword.label')}
           error={errors.confirmPassword?.message}
           variant={errors.confirmPassword?.message ? 'horizontalBorders' : 'fullBorders'}
           mode={'password'}
@@ -137,30 +148,35 @@ export const SignUp = () => {
             )}
           />
           <div className={s.policyText}>
-            <Typography variant={'small_text'}>I agree to the</Typography>
-            <Link href={ROUTES.AUTH.TERMS_OF_SERVICE} passHref legacyBehavior>
-              <Typography variant={'small_link'} as={'a'}>
-                Terms of Service
-              </Typography>
-            </Link>
-            <Typography variant={'small_text'}>and</Typography>
-            <Link href={ROUTES.AUTH.PRIVACY_POLICY} passHref legacyBehavior>
-              <Typography variant={'small_link'} as={'a'}>
-                Privacy Policy
-              </Typography>
-            </Link>
+            {tAuth.rich('policy', {
+              small_text: chunks => <Typography variant={'small_text'}>{chunks}</Typography>,
+              terms_of_service: chunks => (
+                <Link href={ROUTES.AUTH.TERMS_OF_SERVICE} passHref legacyBehavior>
+                  <Typography variant={'small_link'} as={'a'}>
+                    {chunks}
+                  </Typography>
+                </Link>
+              ),
+              privacy_policy: chunks => (
+                <Link href={ROUTES.AUTH.PRIVACY_POLICY} passHref legacyBehavior>
+                  <Typography variant={'small_link'} as={'a'}>
+                    {chunks}
+                  </Typography>
+                </Link>
+              ),
+            })}
           </div>
         </div>
 
         <Button className={s.signUpBtn} type={'submit'} fullwidth disabled={!isValid}>
-          Sign up
+          {tAuth('signUp')}
         </Button>
         <Typography className={s.signInTitle} variant={'regular_text_16'}>
-          Do you have an account?
+          {tAuth('haveAccount')}
         </Typography>
         <Link href={ROUTES.AUTH.SIGN_IN} passHref legacyBehavior>
           <Button className={s.signInBtn} as={'a'} variant={'link'}>
-            Sign In
+            {tCommon('button.logIn')}
           </Button>
         </Link>
       </form>
