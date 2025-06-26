cc0e2ec9715dd70c138be4b9c5c131602dc2c876
Petro Sahal <petrosahal66@gmail.com>
2025-06-27 00:02:17 +0300

    INC1-288 feat: add i18n to EmailVerifiedSuccess

--- src/features/auth/email-verification/email-verified-success/ui/EmailVerifiedSuccess.tsx
+++ src/features/auth/email-verification/email-verified-success/ui/EmailVerifiedSuccess.tsx
@@ -1,4 +1,5 @@
 'use client'
+import { useTranslations } from 'next-intl'
 import Image from 'next/image'
 import Link from 'next/link'
 
@@ -8,17 +9,20 @@ import { ROUTES } from '@/shared/constans'
 import s from './EmailVerifiedSuccess.module.scss'
 
 export const EmailVerifiedSuccess = () => {
+  const t = useTranslations('emailVerifiedSuccess')
+  const tCommon = useTranslations('common')
+
   return (
     <Container className={s.container} width={432} padding={'35px 0 0'}>
       <Typography variant={'h1'} style={{ marginBottom: '20px' }}>
-        Congratulations!
+        {t('title')}
       </Typography>
       <Typography variant={'regular_text_16'} style={{ marginBottom: '54px' }}>
-        Your email has been confirmed
+        {t('description')}
       </Typography>
       <Link href={ROUTES.AUTH.SIGN_IN} passHref legacyBehavior>
         <Button style={{ marginBottom: '72px' }} as={'a'}>
-          Sign in
+          {tCommon('button.logIn')}
         </Button>
       </Link>
 
