88cae84ed6c6699024a354827c9b492cc426af00
Petro Sahal <petrosahal66@gmail.com>
2025-06-26 20:53:30 +0300

    INC1-288 fix: add linear to Header during any request and add ring loaders

--- src/shared/components/header/Header.tsx
+++ src/shared/components/header/Header.tsx
@@ -1,12 +1,18 @@
 'use client'
 
+import { useSelector } from 'react-redux'
+
 import { useTranslations } from 'next-intl'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 
 import { LanguageSelect } from '@/shared/components'
 import { Button, Container, Icon, Typography } from '@/shared/components/ui'
+import Linear from '@/shared/components/ui/loader/linear/Linear'
 import { ROUTES } from '@/shared/constans'
+import { selectAppStatus } from '@/shared/model/appSlice'
+import { colors } from '@/shared/styles/colors'
+import { RequestStatus } from '@/shared/types/Status'
 
 import s from './Header.module.scss'
 
@@ -43,33 +49,37 @@ const NotAuthorizedNavbarControls = () => {
   )
 }
 
-const AuthHeader = () => {
+const AuthHeader = ({ status }: { status: RequestStatus }) => {
   return (
-    <Container width={1310} padding={'0 15px'}>
-      <nav className={s.navbar}>
-        <Typography className={s.logo} as={Link} href={'#'} variant={'large'}>
-          Inctagram
-        </Typography>
-        <LanguageSelect />
-      </nav>
-    </Container>
+    <>
+      {status === 'loading' && <Linear color={colors.accent['500']} height={5} />}
+      <Container width={1310} padding={'0 15px'}>
+        <nav className={s.navbar}>
+          <Typography className={s.logo} as={Link} href={ROUTES.HOME} variant={'large'}>
+            Inctagram
+          </Typography>
+          <LanguageSelect />
+        </nav>
+      </Container>
+    </>
   )
 }
 
 export const Header = ({ isAuthorized }: Props) => {
+  const status = useSelector(selectAppStatus)
   const pathname = usePathname()
 
   const isAuthPage = pathname.includes('/auth')
 
   if (isAuthPage) {
-    return <AuthHeader />
+    return <AuthHeader status={status} />
   }
 
   return (
     <header>
       <Container width={1310} padding={'0 15px'}>
         <nav className={s.navbar}>
-          <Typography className={s.logo} as={Link} href={'#'} variant={'large'}>
+          <Typography className={s.logo} as={Link} href={ROUTES.HOME} variant={'large'}>
             Inctagram
           </Typography>
           <div className={s.authorizedWrapper}>
