e37ea48c582ecd1ae0d313bd77a673b8fcd85053
Elena Kirillova <el-ki-ri@yandex.ru>
2025-06-29 19:58:56 +0400

    INC1-185 feat: add authorization logic

--- src/features/auth/oAuth/ui/GoogleLoginButton.tsx
+++ src/features/auth/oAuth/ui/GoogleLoginButton.tsx
@@ -2,15 +2,30 @@
 
 import { Button, Icon } from '@/shared/components/ui'
 
+import { useOAuth } from '../lib/hooks'
+
 /**
  * A button component for signing in with Google.
  * When clicked, it initiates the Google OAuth login flow.
  */
 
 export const GoogleLoginButton = () => {
+  const { loginWithGoogle } = useOAuth()
+
+  const handleLogin = () => {
+    loginWithGoogle()
+  }
+
   return (
-    <Button variant={'icon'} aria-label={'Войти через Google'}>
-      <Icon iconId={'google'} width={'36px'} height={'36px'} />
-    </Button>
+    <>
+      <Button
+        type={'button'}
+        variant={'icon'}
+        aria-label={'Войти через Google'}
+        onClick={handleLogin}
+      >
+        <Icon iconId={'google'} width={'36px'} height={'36px'} />
+      </Button>
+    </>
   )
 }
