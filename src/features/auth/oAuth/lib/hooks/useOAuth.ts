c6ee7e90ec694decee6c4b2974576c799e2e4c9f
botr1k <a.lavnikevich@gmail.com>
2025-06-30 14:50:49 +0300

    INC1-184 refactor

--- src/features/auth/oAuth/lib/hooks/useOAuth.ts
+++ src/features/auth/oAuth/lib/hooks/useOAuth.ts
@@ -17,7 +17,7 @@ export const useOAuth = () => {
       if (credentialResponse.code) {
         try {
           const result = await googleLogin({
-            redirectUrl: process.env.NEXT_PUBLIC_BASE_URL!, //TODO: change url before deploy
+            redirectUrl: process.env.NEXT_PUBLIC_BASE_URL!,
             code: credentialResponse.code,
           }).unwrap()
 
