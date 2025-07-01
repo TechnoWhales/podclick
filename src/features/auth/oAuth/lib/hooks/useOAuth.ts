fe77840e2a189bc3a78e2e0b7d25e1c5a22bbda6
Elena Kirillova <el-ki-ri@yandex.ru>
2025-07-02 00:41:51 +0400

    INC1-185 refactor: replace env

--- src/features/auth/oAuth/lib/hooks/useOAuth.ts
+++ src/features/auth/oAuth/lib/hooks/useOAuth.ts
@@ -17,7 +17,7 @@ export const useOAuth = () => {
       if (credentialResponse.code) {
         try {
           const result = await googleLogin({
-            redirectUrl: process.env.NEXT_PUBLIC_BASE_URL!,
+            redirectUrl: process.env.NEXT_PUBLIC_BASE_API!,
             code: credentialResponse.code,
           }).unwrap()
 
