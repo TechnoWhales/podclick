418077c775f093b675af1a19ba340c90330f267f
Petro Sahal <petrosahal66@gmail.com>
2025-07-02 16:40:32 +0300

    INC1-308 feat: add option not to add title

--- src/shared/components/ui/modal/Modal.tsx
+++ src/shared/components/ui/modal/Modal.tsx
@@ -10,10 +10,13 @@ import s from './Modal.module.scss'
 type ModalSize = 'lg' | 'md' | 'sm'
 
 type Props = {
-  open: boolean
-  onClose: () => void
-  modalTitle: string
+  open?: boolean
+  onClose?: () => void
+  modalTitle?: string
   size?: ModalSize
+  padding?: string
+  showCloseButton?: boolean
+  modalDescription?: string
 } & ComponentPropsWithoutRef<'div'>
 
 export const Modal = ({
@@ -23,20 +26,23 @@ export const Modal = ({
   children,
   className,
   open,
+  modalDescription,
   ...rest
 }: Props) => (
   <Dialog.Root open={open} onOpenChange={onClose} {...rest}>
     <Dialog.Portal>
       <Dialog.Overlay className={s.overlay} />
       <Dialog.Content className={clsx(s.content, s[size], className)}>
-        <Dialog.Title className={s.title}>{modalTitle}</Dialog.Title>
+        <Dialog.Title className={clsx(s.title, !modalTitle && s.hideTitle)}>{modalTitle && modalTitle}</Dialog.Title>
+        {modalTitle && <>
         <hr />
-        {children}
         <Dialog.Close asChild>
           <Button className={s.iconButton} variant={'icon'}>
             <Icon iconId={'close'} />
           </Button>
         </Dialog.Close>
+        </>}
+        {children}
       </Dialog.Content>
     </Dialog.Portal>
   </Dialog.Root>
