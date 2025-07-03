ecd98f434827c0876585cd9143fefa23be6e1fac
Petro Sahal <petrosahal66@gmail.com>
2025-07-03 19:24:17 +0300

    INC1-241 feat: add cropping

--- src/features/profile/addPhoto/ui/AddPhoto.tsx
+++ src/features/profile/addPhoto/ui/AddPhoto.tsx
@@ -1,5 +1,6 @@
 "use client"
 import { useState, useRef } from 'react'
+import Cropper from "react-easy-crop";
 
 import clsx from 'clsx'
 import Image from 'next/image'
@@ -10,6 +11,9 @@ import { Modal } from '@/shared/components/ui/modal/Modal'
 import s from './AddPhoto.module.scss'
 
 export const AddPhoto = () => {
+  const [crop, setCrop] = useState({ x: 0, y: 0 })
+  const [zoom, setZoom] = useState(1)
+  const [minZoom, setMinZoom] = useState(1)
   const [open, setOpen] = useState(false)
   const fileInput = useRef<HTMLInputElement>(null)
   const [photoPreview, setPhotoPreview] = useState<string | null>(null)
@@ -31,14 +35,39 @@ export const AddPhoto = () => {
 
   return <Modal className={clsx(s.addPhoto, photoPreview && s.cropping )} modalTitle={photoPreview ? '' : 'Add Photo'} open onClose={() => setOpen(!open)}>
     {photoPreview && <div className={s.croppingTitleWrapper}>
-      <div className={s.arrowBack}><Icon  iconId={'arrowIosBack'} /></div>
+      <div className={s.arrowBack}><Icon iconId={'arrowIosBack'} /></div>
       <Typography variant={'h1'}>Cropping</Typography>
       <Button className={s.nextBtn} variant={'link'} >
         {<Typography variant={'h3'} as={'h3'}>Next</Typography>}
       </Button>
     </div>}
     <div className={clsx(s.addPhotoWrapper, photoPreview && s.photoPreview)}>
-      <Image className={clsx(s.photoImg, photoPreview && s.photoPreview)}  src={photoPreview || '/empty-photo.svg'} alt={"Empty photo"} width={222} height={228}/>
+      {photoPreview ? <div className={s.cropContainer}> <Cropper
+              image={photoPreview}
+              crop={crop}
+              zoom={zoom}
+              minZoom={minZoom}
+              maxZoom={5}
+              zoomSpeed={0.5}
+              showGrid={false}
+              cropSize={{ width: 490, height: 500 }}
+              restrictPosition
+              onCropChange={setCrop}
+              onZoomChange={setZoom}
+              style={{cropAreaStyle: {border: 0, boxShadow: "none"}}}
+              onMediaLoaded={({ width, height }) => {
+                const cropWidth = 490
+                const cropHeight = 500
+                const zoomW = cropWidth / width
+                const zoomH = cropHeight / height
+                const requiredZoom = Math.max(zoomW, zoomH)
+
+                setMinZoom(requiredZoom)
+                setZoom(requiredZoom)
+              }}
+          /></div> :
+          <Image className={clsx(s.photoImg, photoPreview && s.photoPreview)}  src={photoPreview || '/empty-photo.svg'} alt={"Empty photo"} width={222} height={228}/>
+      }
       {!photoPreview && <Button className={s.selectBtn} onClick={() => fileInput.current?.click()}>Select from Computer</Button>}
       {!photoPreview && <input type={"file"} accept={"image/*"} style={{display:"none"}} ref={fileInput} onChange={handleFileChange}/>}
       {!photoPreview && <Button className={s.draftBtn} variant={'outlined'}>Open Draft</Button>}
