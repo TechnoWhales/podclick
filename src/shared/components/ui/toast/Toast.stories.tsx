f44e77cd5ce603f189262038bd2044530eedec3c
Elena Kirillova <el-ki-ri@yandex.ru>
2025-07-12 20:55:39 +0400

    INC1-306  docs: add controls and detailed usage docs for ToastContainer

--- src/shared/components/ui/toast/Toast.stories.tsx
+++ src/shared/components/ui/toast/Toast.stories.tsx
@@ -1,27 +1,104 @@
 import type { Meta, StoryObj } from '@storybook/react'
 import { useState } from 'react'
+import { ToastContainer, Zoom } from 'react-toastify'
 
 import { Button } from '@/shared/components/ui/button/Button'
 import { TextField } from '@/shared/components/ui/textfield/TextField'
 import { notify } from '@/shared/lib/notify/notify'
 
-import { ToastProvider } from './Toast'
+const description = `
+**ToastContainer отвечает за отображение всплывающих уведомлений (toast) на странице. <br/> 
+Параметры ToastContainer можно изменять через controls**
 
-const meta: Meta<typeof ToastProvider> = {
+*Пример использования:*
+
+\`\`\`tsx
+<ToastContainer
+  position="bottom-left"
+  autoClose={3000}
+  hideProgressBar
+  transition={Zoom}
+  closeOnClick
+  pauseOnHover
+  draggable
+  theme="colored"
+  icon={false}
+/>
+\`\`\`
+
+**Логика уведомлений находится в модуле \`notify\`**
+
+*Пример использования:*
+
+\`\`\`ts
+notify.success('Успешно!')
+notify.error('Ошибка!')
+notify.info('Информация')
+notify.warning('Предупреждение')
+\`\`\`
+      `
+
+const meta: Meta<typeof ToastContainer> = {
   title: 'Components/Toast',
-  component: ToastProvider,
-} satisfies Meta<typeof ToastProvider>
+  component: ToastContainer,
+  parameters: {
+    docs: {
+      description: {
+        component: description,
+      },
+    },
+  },
+  argTypes: {
+    position: {
+      control: 'select',
+      options: [
+        'top-left',
+        'top-right',
+        'top-center',
+        'bottom-left',
+        'bottom-right',
+        'bottom-center',
+      ],
+      defaultValue: 'bottom-left',
+    },
+    autoClose: {
+      control: 'select',
+      options: [1000, 2000, 3000, 5000, false],
+      defaultValue: 3000,
+    },
+    hideProgressBar: { control: 'boolean', defaultValue: true },
+    icon: { control: 'boolean', defaultValue: true },
+    theme: { control: 'select', options: ['light', 'dark', 'colored'], defaultValue: 'colored' },
+  },
+} satisfies Meta<typeof ToastContainer>
 
 export default meta
 type Story = StoryObj<typeof meta>
 
-export const ToastsDemo: Story = {
-  render: () => {
+export const AllVariants: Story = {
+  args: {
+    position: 'bottom-left',
+    autoClose: 3000,
+    hideProgressBar: true,
+    icon: false,
+    theme: 'colored',
+  },
+  render: args => {
     const [text, setText] = useState('')
 
     return (
       <div>
-        <ToastProvider />
+        <ToastContainer
+          position={args.position}
+          autoClose={args.autoClose}
+          hideProgressBar={args.hideProgressBar}
+          transition={Zoom}
+          closeOnClick
+          pauseOnHover
+          draggable
+          theme={args.theme}
+          icon={args.icon}
+        />
         <div style={{ backgroundColor: '#0d0d0d', padding: '10px' }}>
           <TextField
             type={'text'}
@@ -60,23 +137,5 @@ export const ToastsDemo: Story = {
         </div>
       </div>
     )
-  },
-
-  parameters: {
-    docs: {
-      description: {
-        story: `
-            **Логика уведомлений находится в модуле \`notify\`**
-
-            **Пример использования:**
-            \`\`\`ts
-            notify.success('Успешно!')
-            notify.error('Ошибка!')
-            notify.info('Информация')
-            notify.warning('Предупреждение')
-            \`\`\`
-        `,
-      },
-    },
   },
 }
