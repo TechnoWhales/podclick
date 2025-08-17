356377117a72d7f4eb493110943ae82574d765be
botr1k <a.lavnikevich@gmail.com>
2025-08-17 21:39:23 +0300

    INC1-343 refactor(PublicCard): redesign card layout and image handling
             - Removed PhotoSlider component and commented out its usage
             - Added fixed-size image container with fill layout
             - Restructured info section with new wrapper div
             - Updated styles for better overflow control
             - Temporarily replaced dynamic description with placeholder text
             - Added max-height constraint to card container
             - Improved image styling with object-fit: cover

--- src/features/public-cards/ui/public-card/PublicCard.tsx
+++ src/features/public-cards/ui/public-card/PublicCard.tsx
@@ -5,9 +5,9 @@ import Image from 'next/image'
 import { ExpandableText } from '@/features/public-cards/ui/public-card/expandable-text/ExpandableText'
 import { TimeAgo } from '@/shared/components/time-ago/TimeAgo'
 import { Avatar, Typography } from '@/shared/components/ui'
+import { PhotoSlider } from '@/shared/components/ui/photo-slider/PhotoSlider'
 
 import s from './PublicCard.module.scss'
-import { PhotoSlider } from '@/shared/components/ui/photo-slider/PhotoSlider'
 
 type Props = {
   item: Card
@@ -16,21 +16,31 @@ type Props = {
 export const PublicCard = ({ item }: Props) => {
   return (
     <article className={s.card}>
-      <PhotoSlider>
-        {item.images.map(image => (
-          <Image key={image.createdAt} src={image.url} alt={''} width={234} height={240} />
-        ))}
-      </PhotoSlider>
-      {/*<Image src={item.images[0].url} alt={'placeholder'} width={234} height={240} />*/}
-      <div className={s.postAuthor}>
-        <Avatar url={item.avatarOwner} title={`Avatar ${item.userName}`} />
-        <Typography as={'h3'} variant={'h3'}>
-          {item.userName}
-        </Typography>
+      {/*<PhotoSlider>*/}
+      {/*  {item.images.map(image => (*/}
+      {/*    <Image key={image.createdAt} src={image.url} alt={''} width={234} height={240} />*/}
+      {/*  ))}*/}
+      {/*</PhotoSlider>*/}
+      <div className={s.image}>
+        <Image src={item.images[0].url} alt={'placeholder'} fill style={{ objectFit: 'cover' }} />
       </div>
-      <div>
-        <TimeAgo date={item.createdAt} />
-        <ExpandableText text={item.description} />
+      <div className={s.info}>
+        <div className={s.postAuthor}>
+          <Avatar url={item.avatarOwner} title={`Avatar ${item.userName}`} />
+          <Typography as={'h3'} variant={'h3'}>
+            {item.userName}
+          </Typography>
+        </div>
+        <div>
+          <TimeAgo date={item.createdAt} />
+          {/*<ExpandableText text={item.description} />*/}
+          <ExpandableText
+            text={
+              'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus culpa cumque deleniti enim fugiat in odit officia repellat repellendus vitae.' +
+              'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus culpa cumque deleniti enim fugiat in odit officia repellat repellendus vitae.'
+            }
+          />
+        </div>
       </div>
     </article>
   )
